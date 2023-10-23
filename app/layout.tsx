1c4dbc503ed4b0b9e9018108090a5a5d7e1367c7
Stefano Esposito <stefano.esposito@abstract.it>
2023-10-23 15:20:39 +0200

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
     title: 'Il tuo Senior Dev',
-    description: 'Fai domande tecniche… ti aiuterà',
+    description: 'Fai domande tecniche… ti aiuterà…?',
 }
 
 export default function RootLayout({
