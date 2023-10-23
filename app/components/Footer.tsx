a5d2fb7d838570a132c82164cb63b03dff668516
Stefano Esposito <stefano.esposito@abstract.it>
2023-10-23 15:15:49 +0200

    madeBy link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React from 'react'
 import {HeartFill} from 'react-bootstrap-icons'
+import Link from 'next/link'
+
 
 const Footer = () => {
     return (
         <div className={'footer'}>
-            <small>Made with <HeartFill /> by Stefano Esposito</small>
+            <small>Made with <HeartFill /> by <Link className={'madeBy'} href={'https://www.linkedin.com/in/stefanoesposito75'} target={'_blank'}><strong>Stefano Esposito</strong></Link></small>
         </div>
     )
 }
