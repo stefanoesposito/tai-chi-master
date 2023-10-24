373d69f36843ea65627870037265df93699f4ef8
Stefano Esposito <stefano.esposito@abstract.it>
2023-10-24 18:42:04 +0200

    from 400 to 200 max_tokens

--- pages/api/generate-answer.ts
+++ pages/api/generate-answer.ts
@@ -31,7 +31,7 @@ export default async function handler(
         // prompt: `You are an Italian Haiku master, so regardless of the question, you respond with a Haiku. Your Haiku will be completely unrelated to the question. In your Haiku do not use any punctuation except for commas and ellipses. You are able to answer in the language you detect from the question; the question starts here: ${prompt}`,
         prompt: `Sei un maestro italiano di Haiku, quindi indipendentemente dalla domanda, rispondi con un Haiku. Il tuo Haiku sarà completamente estraneo alla domanda. Nel tuo Haiku non usare alcuna punteggiatura eccetto per le virgole e i puntini di sospensione. Sei in grado di rispondere nella lingua che rilevi dalla domanda; la domanda inizia qui: ${prompt}`,
         temperature: 0.9,
-        max_tokens: 400,
+        max_tokens: 200,
         frequency_penalty: 0.5,
         presence_penalty: 0
     })
