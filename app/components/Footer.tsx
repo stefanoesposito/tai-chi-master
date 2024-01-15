ceae74b2147c47b79630791a29bb793f5f49ce94
Stefano Esposito <stefano.esposito@abstract.it>
2024-01-15 15:58:45 +0100

    from LinkedIn to MyGigz in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 const Footer = () => {
     return (
         <div className={'footer'}>
-            <small>Made with <HeartFill /> by <Link className={'madeBy'} href={'https://www.linkedin.com/in/stefanoesposito75'} target={'_blank'}><strong>Stefano Esposito</strong></Link></small>
+            <small>Made with <HeartFill /> by <Link className={'madeBy'} href={'https://www.mygigz.live/stef'} target={'_blank'}><strong>Stefano Esposito</strong></Link></small>
         </div>
     )
 }
